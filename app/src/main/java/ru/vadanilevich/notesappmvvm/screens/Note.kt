5cf2d1acbc4dfd48cb975bc43f8a395ef08732eb
DanilevichVA <exampl@gmail.com>
2022-12-04 02:07:43 +0300

    lesson 1

---
+++ app/src/main/java/ru/vadanilevich/notesappmvvm/screens/Note.kt
@@ -0,0 +1,9 @@
+package ru.vadanilevich.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Note(navController: NavController){
+
+}
---
+++ app/src/main/java/ru/vadanilevich/notesappmvvm/screens/Start.kt
@@ -0,0 +1,10 @@
+package ru.vadanilevich.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Start(navController: NavController){
+Text(text = "HIHIIIIIIIIIIIIIIIIIIIIIIIII!!!!")
+}
